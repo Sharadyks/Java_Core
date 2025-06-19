53cc7ed501c88528b8b1578469dd406eb834104c
Sharad <ksfbd100@gmail.com>
2025-06-20 00:32:03 +0530

    Added teo new files

---
+++ src/Arrays/ArrayUtility.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArrayUtility {
+    public static int[] inputArray() {
+        System.out.println("Welcome to array sum and average");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the number of elements : ");
+        int size = input.nextInt();
+        int[] nums = new int[size];
+
+        int i = 0;
+        while (i<size){
+            System.out.print("Please enter the number " + (i+1) + " : ");
+            nums[i] = input.nextInt();
+            i++;
+
+        }
+        return nums;
+    }
+
+
+}
---
+++ src/Arrays/SumandAverage.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class SumandAverage {
+    public static void main(String[] args) {
+        int[] numArray = ArrayUtility.inputArray();
+        long sum = sumArray(numArray);
+        int avg = avgArray(numArray);
+        System.out.println("The sum of elements is : " + sum);
+        System.out.println("The average of elements is : " + avg);
+
+    }
+
+    public static long sumArray(int[] numArray){
+        int i = 0;
+        int a = 0;
+        while (i< numArray.length){
+            a += numArray[i];
+            i++;
+        }
+        return a;
+    }
+
+    public static int avgArray(int[] numArray){
+        return 0;
+    }
+}
