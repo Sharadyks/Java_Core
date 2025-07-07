aa190cf1c6ffde535c989fc9921c37f1c9cb6bc7
Sharad <ksfbd100@gmail.com>
2025-07-07 23:09:17 +0530

    Added some files

---
+++ src/Collection_and_generics/wrapper/TestingWrapper.java
@@ -0,0 +1,8 @@
+package Collection_and_generics.wrapper;
+
+public class TestingWrapper {
+    public static void main(String[] args) {
+        Integer a = 59;
+        System.out.println(a.doubleValue());
+    }
+}
