dc433853e7b18f6bcaaa24aedf2a6768fd173a3d
Sharad <ksfbd100@gmail.com>
2025-07-12 22:45:59 +0530

    Added some files

--- src/Multithreading/Locks/BankAccount.java
+++ src/Multithreading/Locks/BankAccount.java
@@ -6,30 +6,35 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class BankAccount {
     private int balance = 100;
-    private final Lock lock = new ReentrantLock();
+    Lock lock = new ReentrantLock();
 
-    public synchronized void withdraw(int amount){
-        System.out.println(Thread.currentThread().getName()+" attempting to withdraw "+ amount);
+    public void withdraw(int amount){
+        System.out.println(Thread.currentThread().getName()+" is preparing withdrwal");
         try {
-            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)){
-                if (balance>=amount){
-                    System.out.println(Thread.currentThread().getName()+" proceeding with withdrawal");
-                    try {
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-                    }
-                    balance -= amount;
-                    System.out.println(Thread.currentThread().getName()+" completed withdrawal. Remaining balance is: "+ balance);
-                } else {
-                    System.out.println(Thread.currentThread().getName() + "Insufficient balance");
-                }
+            if (lock.tryLock(1000,TimeUnit.MILLISECONDS)){
+                if (amount<=balance){
                 try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                        System.out.println(Thread.currentThread().getName()+ " is preceeding with withdrawal");
+                        Thread.sleep(3000);
+                        balance -= amount;
+                        System.out.println(Thread.currentThread().getName()+" withdrawal is done. Remaining balance is "+balance);
+                } catch (Exception e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    lock.unlock();
                 }
+                } else {
+                    System.out.println("Balance is not sufficient to withdraw the amount");
+                }
+
+            } else {
+                System.out.println(Thread.currentThread().getName()+" could not aquire the lock");
             }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        try {
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
