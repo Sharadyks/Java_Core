6988a1d7babd9545c2adb715481fb018558d9699
Sharad <ksfbd100@gmail.com>
2025-08-14 12:08:27 +0530

    Started EXECUTERS FRAMEWORK

---
+++ src/Executer_Framework/First.java
@@ -0,0 +1,41 @@
+package Executer_Framework;
+
+public class First {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        Thread[] threads = new Thread[9];
+        for (int i = 1; i < 10; i++) {
+            int finalI = i;
+            threads[i - 1] = new Thread(
+                    () -> {
+                        long result = fact(finalI);
+                        System.out.println(result);
+                    }
+            );
+            threads[i - 1].start();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+
+            }
+        }
+        System.out.println("Total Time: " + (System.currentTimeMillis() - startTime));
+
+    }
+
+    public static long fact(int n) {
+        try {
+            Thread.sleep(500);
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+        }
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
